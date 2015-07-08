93dd20924787d4df16a41ea7764a820eb2325932
Xavier Gibert <gibert@umiacs.umd.edu>
2015-07-08 20:14:06 +0000

    Increased learning rate to match ICIP paper's

--- examples/amtrak/train_split.sh
+++ examples/amtrak/train_split.sh
@@ -11,9 +11,9 @@ ln -s -f db_text_test$1_lmdb examples/amtrak/db_text_test_lmdb
 ln -s -f db_fast_train$1_lmdb examples/amtrak/db_fast_train_lmdb
 ln -s -f db_fast_test$1_lmdb examples/amtrak/db_fast_test_lmdb
 
-./build/tools/caffe train --gpu=1 --solver=examples/amtrak/amtrak_dual_solver.prototxt
-mv examples/amtrak/net_iter_300000.caffemodel examples/amtrak/net$1_iter_300000.caffemodel
-mv examples/amtrak/net_iter_300000.solverstate examples/amtrak/net$1_iter_300000.solverstate
+./build/tools/caffe train --gpu=0 --solver=examples/amtrak/amtrak_dual_solver.prototxt
+mv examples/amtrak/net_iter_150000.caffemodel examples/amtrak/net$1_iter_150000.caffemodel
+mv examples/amtrak/net_iter_150000.solverstate examples/amtrak/net$1_iter_150000.solverstate
 
 else
     echo "Must specify parameter indicating split idx"
